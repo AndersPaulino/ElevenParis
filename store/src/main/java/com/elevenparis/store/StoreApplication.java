8cb7d709f21d6b6cb9eff66a8649bc30b0ae09bf
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2023-08-09 22:00:43 -0300

    Primeiro Commit do projeto ElevenParis.

---
+++ store/src/main/java/com/elevenparis/store/StoreApplication.java
@@ -0,0 +1,14 @@
+package com.elevenparis.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ store/src/main/java/com/elevenparis/store/entity/AbstractEntity.java
@@ -0,0 +1,35 @@
+package com.elevenparis.store.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false, unique = true)
+    private Long id;
+    @Getter @Setter
+    @Column(name = "ativo", nullable = false)
+    private boolean ativo;
+    @Getter @Setter
+    @Column(name = "registro", nullable = false)
+    private LocalDateTime registro;
+    @Getter @Setter
+    @Column(name = "atualizar")
+    private LocalDateTime atualizar;
+
+    @PrePersist
+    private void prePersist(){
+        this.registro = LocalDateTime.now();
+        this.ativo = true;
+    }
+    @PreUpdate
+    private void preUpdate(){
+        this.atualizar = LocalDateTime.now();
+    }
+}
