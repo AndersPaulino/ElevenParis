db49531c17b0cb64c6755afb995fded2d2b5f1e4
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2023-10-16 20:35:24 -0300

    Correção da Classe Estoque.

--- store/src/main/java/com/elevenparis/store/entity/Estoque.java
+++ store/src/main/java/com/elevenparis/store/entity/Estoque.java
@@ -1,11 +1,12 @@
 package com.elevenparis.store.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_estoque", schema = "public")
@@ -13,4 +14,16 @@ public class Estoque extends AbstractEntity{
     @Getter @Setter
     @Column(name = "cl_nome")
     private String nomeEstoque;
+
+    @Getter @Setter
+    @ManyToMany
+    @JoinTable(name = "cl_estoque.produto",
+            joinColumns = @JoinColumn(
+                    name = "estoque.id"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "produto.id"
+            )
+    )
+    private List<Produto> produto = new ArrayList<>();
 }
