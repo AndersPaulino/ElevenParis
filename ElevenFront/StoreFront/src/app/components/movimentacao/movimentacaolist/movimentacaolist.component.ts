3bb6822bf22ea678a233549b5693660ba1c0efc7
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2023-11-06 21:55:27 -0300

    Correção da Classe Estoque.

--- ElevenFront/StoreFront/src/app/components/movimentacao/movimentacaolist/movimentacaolist.component.ts
+++ ElevenFront/StoreFront/src/app/components/movimentacao/movimentacaolist/movimentacaolist.component.ts
@@ -21,6 +21,7 @@ export class MovimentacaolistComponent {
 
   @Input() movimentacao: Movimentacao = new Movimentacao();
   @Output() retorno = new EventEmitter<Movimentacao>();
+  @Input() modoLancamento: boolean = false;
 
   modalService = inject(NgbModal);
   modalRef!: NgbModalRef;
@@ -47,16 +48,48 @@ export class MovimentacaolistComponent {
     this.modalRef = this.modalService.open(modal, { size: 'sm'});
   }
 
-  addOuEditarMovimentacao(movimentacao: Movimentacao){
+  addOuEditarMovimentacao(movimentacao: Movimentacao) {
     const onComplete = () => {
       this.listAll();
       this.modalRef.dismiss();
     };
-
-    if(movimentacao.id){
+  
+    if (movimentacao.id) {
+      // Atualiza a movimentação existente
       this.movimentacaoService.atualizar(movimentacao.id, movimentacao).subscribe(onComplete);
+      
+      // Calcula o valor total da Movimentação
+      const valorTotal = this.calcularValorTotal(movimentacao);
+      
+      // Calcula o total de produtos da Movimentação
+      const totalProduto = this.calcularTotalProduto(movimentacao);
+      
+      // Faça o que precisar com valorTotal e totalProduto aqui
+      console.log('Valor Total:', valorTotal);
+      console.log('Total de Produtos:', totalProduto);
+    } else {
+      // Se você precisar adicionar alguma lógica adicional para inserir uma nova movimentação,
+      // faça isso aqui
     }
   }
+  
+  // Função para calcular o valor total da Movimentação
+  calcularValorTotal(movimentacao: Movimentacao): number {
+    // Calcula o valor total da Movimentação com base em valorVenda e valorCompra
+    const valorTotal = movimentacao.valorVenda - movimentacao.valorCompra;
+  
+    return valorTotal;
+  }
+  
+  // Função para calcular o total de produtos da Movimentação
+  calcularTotalProduto(movimentacao: Movimentacao): number {
+    // Calcula o total de produtos da Movimentação com base em entrada e saída
+    const totalProduto = movimentacao.entrada - movimentacao.saida;
+  
+    return totalProduto;
+  }
+  
+
   editar(modal: any, movimentacao: Movimentacao, indice: number){
     this.movimentacaoSelecionadoParaEdicao = { ...movimentacao};
     this.indiceSelecionadoParaEdicao = indice;
